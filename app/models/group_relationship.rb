1ee91f8a95415cbd45f827f1c21bc63b06356e79
zyjdong <zyjliuxue@126.com>
2017-04-15 13:53:08 +0800

    implement group relation

--- app/models/group_relationship.rb
+++ app/models/group_relationship.rb
@@ -1,4 +1,2 @@
 class GroupRelationship < ApplicationRecord
-  belongs_to :group
-  belongs_to :user
 end
--- db/migrate/20170415054447_create_group_relationships.rb
+++ db/migrate/20170415054447_create_group_relationships.rb
@@ -2,7 +2,7 @@ class CreateGroupRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :group_relationships do |t|
       t.integer :group_id
-      t.string :user_idinteger
+      t.integer :user_id
 
       t.timestamps
     end
